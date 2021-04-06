8b13b4e31bb6efa636a196e37431f1518e6e61f3
Norma Brur <46872116+Normsss@users.noreply.github.com>
2021-04-06 13:12:35 -0500

    con info

--- scripts.js
+++ scripts.js
@@ -1,31 +1,21 @@
 const insertId = () => {
-    
-    const randomId = Math.floor(Math.random() * 500);
-    const webUrl = `https://api.themoviedb.org/3/movie/${randomId}?api_key=4e370cf87047d952e72f249f236f6e94&language=es`;
-    console.log(webUrl);
-    // const imageUrl = `https://api.themoviedb.org/3/movie/${randomId}/images?api_key=4e370cf87047d952e72f249f236f6e94&language=en-US`
-    // console.log(imageUrl);
+  const randomId = Math.floor(Math.random() * 500);
+  const webUrl = `https://api.themoviedb.org/3/movie/${randomId}?api_key=4e370cf87047d952e72f249f236f6e94&language=es`;
+  console.log(webUrl);
+  // const imageUrl = `https://api.themoviedb.org/3/movie/${randomId}/images?api_key=4e370cf87047d952e72f249f236f6e94&language=en-US`
+  // console.log(imageUrl);
 
-
-    fetch(webUrl)
+  fetch(webUrl)
     .then((response) => response.json())
-    .then(movie => { 
-        poster = movie.poster_path;
-        title = movie.title;
-        year = movie.release_date;
-        country = movie.production_countries[0].name;
+    .then((movie) => {
+      poster = movie.poster_path;
+      title = movie.title;
+      year = movie.release_date;
+      country = movie.production_countries[0].name;
 
-        
-        document.getElementById('output').innerHTML = `<p>${title}</p>` + `<img src="${`https://image.tmdb.org/t/p/w185/${poster}`}" alt="movie-poster"></img>` + `<p class="year">${country}. (${year})</p>`
-
-        })
-        
-} 
-
-
-
-
-
-
-
- 
+      document.getElementById("output").innerHTML =
+        `<p>${title}</p>` +
+        `<img src="${`https://image.tmdb.org/t/p/w185/${poster}`}" alt="movie-poster"></img>` +
+        `<p class="year">${country}. (${year})</p>`;
+    });
+};
