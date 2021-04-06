0bdf95c3a5caedc21e8e80b2d1dc3317b1fee369
Norma Brur <46872116+Normsss@users.noreply.github.com>
2021-04-05 22:46:11 -0500

    con imagenes

--- scripts.js
+++ scripts.js
@@ -1,15 +1,27 @@
 const insertId = () => {
     
-    const randomId = Math.floor(Math.random() * 200);
+    const randomId = Math.floor(Math.random() * 500);
     const webUrl = `https://api.themoviedb.org/3/movie/${randomId}?api_key=4e370cf87047d952e72f249f236f6e94&language=es`;
     console.log(webUrl);
+    const imageUrl = `https://api.themoviedb.org/3/movie/${randomId}/images?api_key=4e370cf87047d952e72f249f236f6e94&language=en-US`
+    console.log(imageUrl);
+
 
     fetch(webUrl)
     .then((response) => response.json())
     .then(movie => { 
-        document.getElementById('output').innerHTML = movie.title;
+        poster = movie.poster_path;
+        title = movie.title;
+        document.getElementById('output').innerHTML = `<p>${title}</p>` + `<img src="${`https://image.tmdb.org/t/p/w185/${poster}`}" alt="movie-poster"></img>`
+        })
+        
+    fetch(imageUrl)
+    .then((response) => response.json())
+    .then(movieImage => {
+
     })
-}
+} 
+
 
 
 
