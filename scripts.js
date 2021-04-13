a0ab9faf087b819394f0e875464fbc4dd32c0ec4
Norma Brur <46872116+Normsss@users.noreply.github.com>
2021-04-13 12:03:28 -0500

    separando funciones

--- scripts.js
+++ scripts.js
@@ -5,17 +5,20 @@ const insertId = () => {
   // const imageUrl = `https://api.themoviedb.org/3/movie/${randomId}/images?api_key=4e370cf87047d952e72f249f236f6e94&language=en-US`
   // console.log(imageUrl);
 
-  fetch(webUrl)
-    .then((response) => response.json())
-    .then((movie) => {
-      poster = movie.poster_path;
-      title = movie.title;
-      year = movie.release_date;
-      country = movie.production_countries[0].name;
+  getMovie(webUrl).then((movie) => {
+    const poster = movie.poster_path;
+    const title = movie.title;
+    const year = movie.release_date;
+    const country = movie.production_countries[0].name;
 
-      document.getElementById("output").innerHTML =
-        `<p>${title}</p>` +
-        `<img src="${`https://image.tmdb.org/t/p/w185/${poster}`}" alt="movie-poster"></img>` +
-        `<p class="year">${country}. (${year})</p>`;
-    });
+    document.getElementById("output").innerHTML = `
+        <p>${title}</p>
+        <img src="https://image.tmdb.org/t/p/w185/${poster}" alt="movie-poster"></img>
+        <p class="year">${country}. (${year})</p>
+      `;
+  });
+};
+
+const getMovie = (webUrl) => {
+  return fetch(webUrl).then((response) => response.json());
 };
